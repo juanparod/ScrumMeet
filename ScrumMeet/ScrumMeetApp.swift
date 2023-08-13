d581ae3f680f1e8c6615aaa0eb5eae967c04fb83
Juan Pablo Rodriguez <jrodriguez79@gmail.com>
2023-08-12 21:42:41 -0600

    Initial Commit

---
+++ ScrumMeet/ScrumMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumMeetApp.swift
+//  ScrumMeet
+//
+//  Created by Juan Pablo Rodriguez on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
